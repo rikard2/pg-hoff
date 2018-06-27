rikard2/pg-hoff

commit e5df93600b682b2027b6c1c4b61756f597ea8d63

Hoff import
Copy data between servers by using the hoff import language ;)

lib/pg-hoff.coffee
@@ -114,7 +114,7 @@ module.exports = PgHoff =
             @provider = new PgHoffAutocompleteProvider()
 
         @subscriptions = new CompositeDisposable
-        @subscriptions.add atom.commands.add 'atom-workspace', 'pg-hoff:goto-declaration': => @gotoDeclaration()
+        @subscriptions.add atom.commands.add 'atom-workspace', 'pg-hoff:perform-hoff-import': => @executeHoffImport()
         @subscriptions.add atom.commands.add 'atom-workspace', 'pg-hoff:connect': => @connect()
         @subscriptions.add atom.commands.add 'atom-workspace', 'pg-hoff:search-history': => @searchHistoryWithConnect()
         @subscriptions.add atom.commands.add 'atom-workspace', 'pg-hoff:stop-query': => @stopQuery()
@@ -522,6 +522,16 @@ module.exports = PgHoff =
             @connect()?.then => @executeQuery(cursor_pos)
 
     executeQuery: (cursor_pos) ->
+        @executePost('query', null, cursor_pos)
+
+    executeHoffImport: () ->
+        editor = atom.workspace.getActivePaneItem()
+        file = editor?.buffer.file
+        request =
+            hoffimportfile: file['path']
+        @executePost('hoff_import', request, null)
+
+    executePost: (path, request, cursor_pos) ->
         if @processingBatch? and @processingBatch
             atom.notifications.addWarning('Execution in progress, hold on!')
             return
@@ -545,13 +555,12 @@ module.exports = PgHoff =
                 selectedText = atom.workspace.getActiveTextEditor().getText().trim()
             else
                 return
-        request =
+        request = request ?
             query: selectedText
             alias: alias
         if cursor_pos
             request.cursor_pos = cursor_pos
-
-        return PgHoffServerRequest.Post('query', request)
+        return PgHoffServerRequest.Post(path, request)
             .then (response) ->
                 if response.statusCode == 500
                     throw("/query status code 500")

lib/bottom-dock/results-pane.coffee
@@ -134,14 +134,14 @@ class ResultsPaneView extends DockPaneView
             range: null
             marker: null
         }
+        try
+            if (JSON.stringify bufferRange.start).toString() == (JSON.stringify bufferRange.end).toString()
+                bufferRange = atom.workspace.getActiveTextEditor().getBuffer().getRange()
+            if queryNumber > 1
+                bufferRange.start = @processedQueries[queryNumber-2].range.end
 
-        if (JSON.stringify bufferRange.start).toString() == (JSON.stringify bufferRange.end).toString()
-            bufferRange = atom.workspace.getActiveTextEditor().getBuffer().getRange()
-        if queryNumber > 1
-            bufferRange.start = @processedQueries[queryNumber-2].range.end
-
-        atom.workspace.getActiveTextEditor().scanInBufferRange(new RegExp(@escapeRegExp(result.query)), bufferRange, (hit) => @queryHit(hit, queryInfo) )
-
+            atom.workspace.getActiveTextEditor().scanInBufferRange(new RegExp(@escapeRegExp(result.query)), bufferRange, (hit) => @queryHit(hit, queryInfo) )
+        catch
     updateRendering: (result) ->
         for query in @processedQueries
             if query.queryId == result.queryid

lib/query.coffee
@@ -5,14 +5,21 @@ class PgHoffQuery
     serialize: ->
     destroy: ->
 
+    @HoffImport: (hoffimportfile) ->
+        request =
+            hoffimportfile: hoffimportfile
+        return Perform_request('hoff_import', request)
+
     @Execute: (query, alias) ->
         request =
             query: query
             alias: alias ? atom.workspace.getActivePaneItem().alias
+        return @Perform_request('query', request)
 
+    @Perform_request: (path, request) ->
         url = null
         return PgHoffServerRequest
-            .Post('query', request)
+            .Post(path, request)
             .then (response) ->
                 # #console.log 'query response', response
                 if response.statusCode == 500

commit 65571eb75f7adf37580a104f7d9448edc888f636

Fix goto-declaration broken in e5df936

lib/pg-hoff.coffee
@@ -114,6 +114,7 @@ module.exports = PgHoff =
             @provider = new PgHoffAutocompleteProvider()
 
         @subscriptions = new CompositeDisposable
+        @subscriptions.add atom.commands.add 'atom-workspace', 'pg-hoff:goto-declaration': => @gotoDeclaration()
         @subscriptions.add atom.commands.add 'atom-workspace', 'pg-hoff:perform-hoff-import': => @executeHoffImport()
         @subscriptions.add atom.commands.add 'atom-workspace', 'pg-hoff:connect': => @connect()
         @subscriptions.add atom.commands.add 'atom-workspace', 'pg-hoff:search-history': => @searchHistoryWithConnect()